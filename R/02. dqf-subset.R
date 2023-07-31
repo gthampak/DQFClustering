0df510a4b5411d08a05a636ceecc230f3c34b7fc
Guy_T <gthampak@gmail.com>
2023-07-31 17:27:35 +0700

    fixed dependecies

--- R/02. dqf-subset.R
+++ R/02. dqf-subset.R
@@ -1,22 +1,23 @@
-## ----setup, include=FALSE--------------------------------------------------------------
+## ----setup, include=FALSE------------------------------------------------------------------------------------------------------------------------------
 knitr::opts_chunk$set(echo = TRUE)
 
 
-## --------------------------------------------------------------------------------------
+## ---- eval=FALSE, include=FALSE,results='hide'---------------------------------------------------------------------------------------------------------
+## source("00. datasets.R")
+## source("01. dqf-outlier.R")
 
 
-
-## ---- eval=FALSE, include=FALSE,results='hide'-----------------------------------------
+## ---- eval=FALSE, include=FALSE,results='hide'---------------------------------------------------------------------------------------------------------
 ## knitr::purl("02. dqf-subset.Rmd")
 
 
-## --------------------------------------------------------------------------------------
+## ------------------------------------------------------------------------------------------------------------------------------------------------------
 dqf.subset <- function(data = NULL, gram.mat = NULL, g.scale=2, angle=c(45), kernel="linear", p1=1, p2=0, n.splits=100, subsample=50, z.scale=TRUE, k.w=3, adaptive=TRUE, G="norm") {
   # kernelized version of depthity
-  #
-  # inputs:
+  # 
+  # inputs: 
   #   data (matrix or data frame) - a data matrix of explanatory variables
-  #   kernel - of form "linear", "rbf" or "poly", or a user defined function
+  #   kernel - of form "linear", "rbf" or "poly", or a user defined function 
   #   g.scale (scalar) - scales the base distribution G
   #   angle (numeric vector of length 3)- angles of cone from midline, must live between 0 and 90
   #   p1 - first parameter for kernel
@@ -35,24 +36,24 @@ dqf.subset <- function(data = NULL, gram.mat = NULL, g.scale=2, angle=c(45), ker
     param1 <- 0; param2 <- 1 }
   if (G=="unif") {
     param1 <- -1; param2 <- 1 }
-  if (is.null(data) & is.null(gram.mat))
+  if (is.null(data) & is.null(gram.mat)) 
     stop("Either a data set or Gram matrix must be provided")
-  if (min(angle) <= 0 | max(angle) >= 90)
+  if (min(angle) <= 0 | max(angle) >= 90) 
     stop("Angles must be between 0 and 90")
   if (is.null(data))
     n.obs <- nrow(gram.mat)
   if (is.null(gram.mat))
     n.obs <- nrow(data)
-
-
+  
+  
   #scram <- sample(n.obs)
   #unscram <- c()
   #for(i in 1:length(scram)){
   #  unscram <- c(unscram,which(scram==i))
   #}
-
+  
   pairs <- subsamp.dqf(n.obs, subsample)
-  if (is.null(gram.mat)) {
+  if (is.null(gram.mat)) {  
     if (z.scale==TRUE)
       data <- apply(data, 2, scale) #z-scale data
     if (is.function(kernel)==TRUE)
@@ -64,7 +65,7 @@ dqf.subset <- function(data = NULL, gram.mat = NULL, g.scale=2, angle=c(45), ker
     if (kernel == "rbf") {
       kern <- function(x,y)
         return(exp(-sum((x-y)^2)/p1))
-    }
+    }             
     if (kernel == "poly") {
       kern <- function(x,y)
         return((sum(x*y)+p2)^p1)
@@ -91,34 +92,34 @@ dqf.subset <- function(data = NULL, gram.mat = NULL, g.scale=2, angle=c(45), ker
   norm.k2 <- error.k <- k.to.mid <- rep(0, n.obs)
   dep1 <- matrix(0, nrow=nrow(pairs), ncol=n.splits)
   qfs1 <- matrix(0, nrow=nrow(pairs), ncol=100)
-
+  
   ret.pairs <- matrix(0,nrow=nrow(pairs),ncol=2)
   k.to.mids <- matrix(ncol = n.obs, nrow = nrow(pairs))
-
+  
   ret.gs <- matrix(ncol = n.obs, nrow = n.splits)  # skeleton for goods data.frame
   ret.goods <- list()
-
+  
   # pairs.goods <- list(pairs=list(c(0,0)),goods=list(data.frame(matrix(ncol = n.obs, nrow = 100)))) # create list of pairs and their corresponding 100 (num.splits) good vectors
-
+  
   for (i.subs in 1:nrow(pairs)) {
     i <- pairs[i.subs,1];  j <- pairs[i.subs,2]
-
+    
     # pairs.goods$pairs <- append(pairs.goods$pairs,list(c(scram[i],scram[j]))) # record pairs of points (original indices)
-
+    
     #ret.pairs[i.subs,] <- c(scram[i],scram[j])
     ret.pairs[i.subs,] <- c(i,j)
-
+    
     for (k in 1:n.obs) {
       norm.k2[k] <- gram[k,k] + 1/4*(gram[i,i]+gram[j,j]) + 1/2*gram[i,j]-gram[k,i]-gram[k,j]
       k.to.mid[k] <- (gram[k,i]-gram[k,j]+1/2*(gram[j,j]-gram[i,i]))/sqrt(gram[i,i]+gram[j,j]-2*gram[i,j]) # return this
       error.k[k] <- sqrt(abs(norm.k2[k] - k.to.mid[k]^2))
     }
-
+    
     k.to.mids[i.subs,] <- k.to.mid
-
+    
     for (c in 1:length(splits)) {
       good <- rep(1, n.obs)
-      s <- splits[c] * (sd.w(k.to.mid, k.w)*adaptive + (adaptive==FALSE))
+      s <- splits[c] * (sd.w(k.to.mid, k.w)*adaptive + (adaptive==FALSE)) 
       good[k.to.mid/s > 1] <- 0  #points on other side of cone tip removed
       d.to.tip <- abs(k.to.mid - s)
       good1 <- good * (abs(atan(error.k / d.to.tip)) < (angle[1]/360*2*pi))  #points outside of cone removed
@@ -133,10 +134,10 @@ dqf.subset <- function(data = NULL, gram.mat = NULL, g.scale=2, angle=c(45), ker
   for (i in 1:n.obs) {
     dqf1[i,] <- apply(qfs1[which(pairs[,1]==i | pairs[,2]==i),],2,mean, na.rm=TRUE)
   }
-
+  
   # reorder
   # dqf1 <- dqf1[unscram,]  #map back to original indicies
-
+  
   return(list(dqf1=dqf1,ret.pairs=ret.pairs,ret.goods=ret.goods,k.to.mids=k.to.mids))
 }
 
